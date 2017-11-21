74072413ecdbce831d022ad74235a54240828686
David Fletcher <fletchdav@gmail.com>
2017-11-21 17:23:37 +0100

    okaaaaaaay

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
+10.times do
+  Article.create(title: Faker::Hipster.sentence, content: Faker::Lorem.paragraphs)
+end
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
